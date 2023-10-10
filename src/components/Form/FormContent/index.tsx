21d5bed9a74aa29ba7b6019b7ddd5fbac583e0e6
Miloš <m.naskovic@shindiristudio.com>
2023-10-10 15:52:28 +0200

    feature: created form content

---
+++ src/components/Form/FormContent/index.tsx
@@ -0,0 +1,14 @@
+import ListItem from './ListItem'
+import './index.css'
+
+export default function FormContent() {
+  return (
+    <div className='content'>
+        <h1 className='content-header'>Stay updated!</h1>
+        <p className='content-paragraph'>Join 60,00+ product managers receiving monthly updates on:</p>
+        <ListItem text='Product discovery and building what matters'/>
+        <ListItem text='Measuring to ensure updates are a success'/>
+        <ListItem text='And much more!'/>
+    </div>
+  )
+}
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,12 +2,14 @@
 import "./index.css";
 
 // Component
-
+import FormContent from "./FormContent";
+import FormIlustration from "./FormIlustration";
 
 export default function Form() {
   return (
   <div className="form-container">
-   
+    <div className="content-container"><FormContent /></div>
+   <FormIlustration/>
   </div>
   )
 }
